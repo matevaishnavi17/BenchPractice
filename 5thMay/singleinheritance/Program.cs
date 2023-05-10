7bdb34b724daf8c713c214e756fdce760ae63bbd
Vaishnavi Ganesh Mate <vaishnavi.ganesh-mate@capgemini.com>
2023-05-10 18:21:15 +0530

    5th may

---
+++ 5thMay/singleinheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace singleinheritance
+{
+
+    public class Employee  
+    {  
+       public float salary = 40000;  
+    }  
+    
+    public class Programmer: Employee  
+    {  
+       public float bonus = 10000;  
+    }  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();  
+  
+            Console.WriteLine("Salary: " + p1.salary);  
+            Console.WriteLine("Bonus: " + p1.bonus);  
+        }
+    }
+}
