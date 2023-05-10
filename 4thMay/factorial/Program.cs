49aec189a26a0ee593304c92ac094b8f843ba361
Vaishnavi Ganesh Mate <vaishnavi.ganesh-mate@capgemini.com>
2023-05-10 17:26:58 +0530

    3rd may

---
+++ 4thMay/factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,fact=1,number;      
+            Console.Write("Enter any Number: ");      
+            number= int.Parse(Console.ReadLine());     
+            for(i=1;i<=number;i++){      
+                fact=fact*i;      
+            }      
+            Console.Write("Factorial of " +number+" is: "+fact); 
+        }
+    }
+}
---
+++ 4thMay/swapnumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace swapnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  a=5, b=10;            
+            Console.WriteLine("Before swap a= "+a+" b= "+b);    
+            a=a+b; //a=15 (5+10)      
+            b=a-b; //b=5 (15-10)      
+            a=a-b; //a=10 (15-5)   
+            Console.Write("After swap a= "+a+" b= "+b); 
+        }
+    }
+}
